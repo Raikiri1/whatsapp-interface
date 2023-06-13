ab78b7e966125bd9e08d48a1c5058cd9c6450de3
raikiri1 <aibecka2@gmail.com>
2023-06-13 11:43:27 +0600

    Updates

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import loginSlice, { loginReducer } from './slice/loginSlice'
-import chatsSlice from './slice/chatsSlice'
-export const store = configureStore({
-  reducer: {
-    login: loginReducer,
-      chatsSlice,
-  },
-})
